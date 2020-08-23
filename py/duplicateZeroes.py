b522d3ddabd2e92a40c51d719795b464adf9979c
Ivaylo-Kirov <ivo.kirov@gmail.com>
2020-08-23 13:40:14 -0400

    Fix to duplicate zeroes

--- py/duplicateZeroes.py
+++ py/duplicateZeroes.py
@@ -2,11 +2,16 @@
 def duplicateZeroes(arr):
     i = 0
 
-    while i < len(arr) - 1:
+    initLen = len(arr) - 1
+
+    while i <= initLen:
         if arr[i] == 0:
-            arr = arr[:i+1] + [0] + arr[i+1:len(arr) - 1]
+            arr = arr[:i+1] + [0] + arr[i+1:initLen + 1]
             i += 1
+            initLen += 1
         i += 1
+    # there is a bug here as length remains the same after a new zero is added, so the last few items get deleted
+    # idea is to set initial length as a variable, and then increase it manually az zeroes are added
 
 
 duplicateZeroes([1,0,2,3,0,4,5,0])
